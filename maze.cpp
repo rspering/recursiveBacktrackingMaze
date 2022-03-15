9cba4e562441df6947ee7ac36aa161b1c5466b4a
Michael C <mwcxpt@umsystem.edu>
2022-03-14 19:14:10 -0500

    Initial

---
+++ maze.cpp
@@ -0,0 +1,47 @@
+/* Here in the .cpp you should define and implement everything declared in the .h file.
+ */
+
+#include "maze.h"
+
+
+void get_identity(string &my_id)
+{
+}
+
+
+string * build_matrix(int rows)
+{
+}
+
+
+void fill_matrix(string *matrix, int rows)
+{
+}
+
+
+void print_matrix(string *matrix, int rows)
+{
+}
+
+void delete_matrix(string *&matrix)
+{
+}
+
+
+void find_start(string *matrix, int rows, int &row, int &col)
+{
+}
+
+bool find_exit(string *matrix, int row, int col)
+{
+}
+
+
+bool at_end(string *matrix, int row, int col)
+{
+}
+
+bool valid_move(string *matrix, int row, int col, string direction)
+{
+}
+
---
+++ pa03.cpp
@@ -0,0 +1,15 @@
+/* Declare any non-required functions above main.
+ * The duty of main here is to interact with the user, take in input, and manage wrapping output and runtime.
+ * Remember, if you are considering putting something in main or a function, double check the specifications.
+ * Each function should only do what it is specified to do, and no more.
+ */
+
+#include "maze.h"
+
+
+int main()
+{
+
+    return 0;
+}
+
